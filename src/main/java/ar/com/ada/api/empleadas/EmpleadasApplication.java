3284a693439aff2bd82a9e0bdc436c19a2245b7e
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-06-19 19:04:59 -0300

    primer commit

---
+++ src/main/java/ar/com/ada/api/empleadas/EmpleadasApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.empleadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleadasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleadasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/ada/api/empleadas/EmpleadasApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.empleadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleadasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
